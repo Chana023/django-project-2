8b460234c1cfc6c71ce83880a8f5b16f1088ee2b
Jesal <jesalchana23@gmail.com>
2022-05-21 21:28:09 +0200

    Added super-user and registered django models with admin

--- scrumapp/admin.py
+++ scrumapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from scrumapp.models import Task
+from scrumapp.models import User
+from scrumapp.models import User_Story
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(User_Story)
