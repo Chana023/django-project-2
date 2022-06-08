b41c84cc765ce20503ab26963919d6a548464322
Jesal <jesalchana23@gmail.com>
2022-06-08 19:37:48 +0200

    Added fixes given by Brendon so login view is stable

--- scrumapp/urls.py
+++ scrumapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login_page, name='login'),
+    path('login', views.login_page, name='index'),
+    path('task', views.TemplateTaskView.as_view(), name='task_view')
 ]
--- scrumapp/views.py
+++ scrumapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView 
 # Create your views here.
 def index(request):
     return HttpResponse("Testing")
+
+def login_page(request):
+    return render(request, 'scrumapp/login.html')
+
+class TemplateTaskView(TemplateView):
+    template_name = 'scrumapp/task.html'
+
+    def get_context_data(self, **kwargs):
+        #kwargs.setdefault('view', self)
+        #if self.extra_context is not None:
+        #    kwargs.update(self.extra_context)
+        #return kwargs
+        kwargs = super().get_context_data(**kwargs)
+        kwargs['Tasks'] = ['1','2','3']
+        return kwargs
